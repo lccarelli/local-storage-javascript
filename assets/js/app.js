d801659e346965e97511eb16dde4df83201fff7e
Lau Carelli <carelli.lc@gmail.com>
2018-05-03 17:46:03 -0300

    funcionalidad, agregar tweet a la lista de tweets

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,26 @@ const listaTweets = document.getElementById('lista-tweets');
 
 //Event Listener
 
-evenListener();
+eventListener();
 
-function evenListener () {};
+function eventListener () {
+     //cuando se envía el formulario
+     document.getElementById('formulario').addEventListener('submit', agregarTweet);
+}
 
 //Funciones
+
+//añadir tweet del formulario
+function agregarTweet(e){
+     e.preventDefault();
+     console.log('formulario enviado');
+     
+     //leer el valor del text área
+     const tweet = document.getElementById('tweet').value;
+     console.log(tweet);
+
+     //crear elemento y añadirlo a la lista
+     const li = document.createElement('li');     //creando el li
+     li.innerText = tweet;    //agregadno el contenido del tweet al li
+     listaTweets.appendChild(li);  //agregando el li a listaTweets
+}
